f852689b79899e3f0c09eca1cc99bb47ecc020fa
doncov.eugene <doncov.eugene@alphaopen.com>
2019-02-26 14:58:32 +0300

    cosmetic

--- ESP8266/src/utils.h
+++ ESP8266/src/utils.h
@@ -1,11 +1,16 @@
+#ifndef _WATERIUS_INIT_h
+#define _WATERIUS_INIT_h
+
 #include "string.h"
 
 /*
 Запишем 0 в конце буфера принудительно.
 */
+
 inline void strncpy0(char *dest, const char *src, const size_t len)
 {   
     strncpy(dest, src, len-1);
     dest[len-1] = '\0';
-}
-        
+} 
+
+#endif
