3e4fb1388d1c92027f10f68496a66726d4b33f0d
IAmYosya <m9y.yosya@gmail.com>
2019-03-18 02:37:57 +0300

    (UserAuth): Refactor
    - Remove reg function
    - Refactor login function

--- ESP8266/src/UserAuth.cpp
+++ ESP8266/src/UserAuth.cpp
@@ -5,15 +5,16 @@
 #include "Logging.h"
 #include <ArduinoJson.h>
 
+
 #define JSON_BUFFER_SIZE 1000
-#define REG_REQUEST_URL "http://192.168.199.2:8020/reg"
-#define LOGIN_REQUEST_URL "http://192.168.199.2:8020/login"
+#define LOGIN_REQUEST_URL "http://192.168.199.11:8020/login"
+
 
 bool UserAuth::login(char *email, char *token)
 {
     LOG_NOTICE("LGN", "START :: Login");
     LOG_INFO("LGN", "Email: " << email);
-    LOG_INFO("LGN", "Token: " << email);
+    LOG_INFO("LGN", "Token: " << token);
     
     // Try to login
     String jsonBody;
@@ -22,41 +23,11 @@ bool UserAuth::login(char *email, char *token)
     root["email"] = email;
     root["token"] = token;
     root.printTo(jsonBody);
-    bool login_result = WateriusHttps::sendGetRequest(LOGIN_REQUEST_URL);
+    WateriusHttps::ResponseData responseData = WateriusHttps::sendPostRequestWithJson(LOGIN_REQUEST_URL, jsonBody);
+    bool login_result = responseData.isSuccess && responseData.code == 200;
 
     LOG_INFO("LGN", "Login result -- " << (login_result ? "SUCCESS" : "ERROR"));
     LOG_NOTICE("LGN", "END :: Login");
 
     return login_result;
-}
-
-bool UserAuth::reg(char *email)
-{
-    LOG_NOTICE("REG", "START :: Registration");
-    LOG_INFO("REG", "Email: " << email);
-    
-    // Generate token
-    char *token;
-    randomSeed(time(nullptr));
-    int salt = rand();
-    auto x = BearSSL::HashSHA256();
-    x.add(email, sizeof(email));
-    x.add(&salt, sizeof(salt));
-    x.end();
-    token = (char *)x.hash();
-    LOG_INFO("REG", "Token: " << token);
-    
-    // Register
-    String jsonBody;
-    StaticJsonBuffer<JSON_BUFFER_SIZE> jsonBuffer;
-    JsonObject& root = jsonBuffer.createObject();
-    root["email"] = email;
-    root["token"] = token;
-    root.printTo(jsonBody);
-    bool reg_result = WateriusHttps::sendPostRequestWithJson(REG_REQUEST_URL, jsonBody);
-
-    LOG_INFO("REG", "Registration result -- " << (reg_result ? "SUCCESS" : "ERROR"));
-    LOG_NOTICE("REG", "END :: Registration");
-
-    return reg_result;
 }
