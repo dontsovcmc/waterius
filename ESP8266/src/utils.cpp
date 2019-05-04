5ef9d5cac922805f787bac252055db4d41bba2d1
Dontsov Evgeny <don-and-home@mail.ru>
2019-05-04 03:10:33 +0300

    3 servers for setClock works

--- ESP8266/src/utils.cpp
+++ ESP8266/src/utils.cpp
@@ -5,26 +5,35 @@
 #include "string.h"
 #include "time.h"
 
+#define NTP_CONNECT_TIMEOUT 3000UL
 
-bool setClock() 
+bool setClock(const char* ntp_server)
 {
-	configTime(0, 0, "ru.pool.ntp.org", "time.nist.gov");
+	configTime(0, 0, ntp_server);
 
 	LOG_NOTICE("NTP", "Waiting for NTP time sync: ");
 	uint32_t start = millis();
 	time_t now = time(nullptr);
 	
-	while (now < 8 * 3600 * 2  && millis() - start < ESP_CONNECT_TIMEOUT) {
-		delay(500);
+	while (now < 8 * 3600 * 2  && millis() - start < NTP_CONNECT_TIMEOUT) {
+		delay(100);
 		now = time(nullptr);
 	}
 	
-	if (millis() - start >= ESP_CONNECT_TIMEOUT) {
-		return false;
-	}
+	return millis() - start < NTP_CONNECT_TIMEOUT;
+}
 
-	struct tm timeinfo;
-	gmtime_r(&now, &timeinfo);
-	LOG_NOTICE("NTP", "Current time: " << asctime(&timeinfo));
-	return true;
+bool setClock() 
+{
+	if (setClock("1.ru.pool.ntp.org") 
+	    || setClock("2.ru.pool.ntp.org")
+	    || setClock("pool.ntp.org")) {
+
+		time_t now = time(nullptr);
+		struct tm timeinfo;
+		gmtime_r(&now, &timeinfo);
+		LOG_NOTICE("NTP", "Current time: " << asctime(&timeinfo));
+		return true;
+	}
+	return false;
 }
