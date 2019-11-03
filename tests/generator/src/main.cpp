c72b1ad39475802d5af8a6c7d7704a6c132c08e7
Dontsov Evgeny <don-and-home@mail.ru>
2019-11-03 17:47:26 +0300

    generator

--- tests/generator/src/main.cpp
+++ tests/generator/src/main.cpp
@@ -2,57 +2,106 @@
 
 #include "TaskScheduler.h"
 
-#define OUTPUT1_PIN LED_BUILTIN
-//#define OUTPUT2_PIN 2
+#define OUTPUT1_PIN LED_BUILTIN  // 13
+#define OUTPUT2_PIN 12
 
-#define RETRIES 5
+#define RETRIES 100
 
-#define IMPULSE_MS_MIN 100
-#define IMPULSE_MS_MAX 500
+#define IMPULSE_MS_MIN 500
+#define IMPULSE_MS_MAX 5000
 
-#define WAIT_MS_MIN 999
-#define WAIT_MS_MAX 1001
+#define WAIT_MS_MIN 1500
+#define WAIT_MS_MAX 5000
 
 void WrapperStart();
-void ImpulseStart();
-void ImpulseStop();
+void ImpulseStart1();
+void ImpulseStop1();
+void ImpulseStart2();
+void ImpulseStop2();
+void TurnOn(uint8_t pin);
+void TurnOff(uint8_t pin);
 
 Scheduler ts;
 
+uint32_t count1 = 0;
+uint32_t count2 = 0;
+
 Task tStart(0, TASK_ONCE, &WrapperStart, &ts, true);
-Task tImpulseOn(0, TASK_ONCE, &ImpulseStart, &ts, false);
-Task tImpulseOff(0, TASK_ONCE, &ImpulseStop, &ts, false);
+Task tImpulseOn1(0, TASK_ONCE, &ImpulseStart1, &ts, false);
+Task tImpulseOff1(0, TASK_ONCE, &ImpulseStop1, &ts, false);
+Task tImpulseOn2(0, TASK_ONCE, &ImpulseStart2, &ts, false);
+Task tImpulseOff2(0, TASK_ONCE, &ImpulseStop2, &ts, false);
 
 void WrapperStart()
 {	
 	Serial.print(millis());
 	Serial.print(" : ");
 	Serial.println("Wrapper");
-	tImpulseOn.restartDelayed(WAIT_MS_MIN + random(WAIT_MS_MAX-WAIT_MS_MIN));
+	tImpulseOn1.restartDelayed(WAIT_MS_MIN + random(WAIT_MS_MAX-WAIT_MS_MIN));
+	tImpulseOn2.restartDelayed(WAIT_MS_MIN + random(WAIT_MS_MAX-WAIT_MS_MIN));
 }
 
-void ImpulseStart()
+void ImpulseStart1()
+{
+	TurnOn(OUTPUT1_PIN);
+	tImpulseOff1.restartDelayed(IMPULSE_MS_MIN + random(IMPULSE_MS_MAX-IMPULSE_MS_MIN));
+}
+
+void ImpulseStop1()
+{
+	TurnOff(OUTPUT1_PIN);
+	if (++count1 < RETRIES)
+	{
+		tImpulseOn1.restartDelayed(WAIT_MS_MIN + random(WAIT_MS_MAX-WAIT_MS_MIN));
+	}
+}
+
+void ImpulseStart2()
+{
+	TurnOn(OUTPUT2_PIN);
+	tImpulseOff2.restartDelayed(IMPULSE_MS_MIN + random(IMPULSE_MS_MAX-IMPULSE_MS_MIN));
+}
+
+void ImpulseStop2()
+{
+	TurnOff(OUTPUT2_PIN);
+	if (++count2 < RETRIES)
+	{
+		tImpulseOn2.restartDelayed(WAIT_MS_MIN + random(WAIT_MS_MAX-WAIT_MS_MIN));
+	}
+}
+
+void TurnOn(uint8_t pin)
 {
 	Serial.print(millis());
 	Serial.print(" : ");
-	Serial.println("ImpulseStart");
-	digitalWrite(OUTPUT1_PIN, HIGH);
-	tImpulseOff.restartDelayed(IMPULSE_MS_MIN + random(IMPULSE_MS_MAX-IMPULSE_MS_MIN));
+	Serial.print("ON ");
+	Serial.println(pin);
+	digitalWrite(pin, HIGH);
 }
 
-void ImpulseStop()
+void TurnOff(uint8_t pin)
 {
 	Serial.print(millis());
 	Serial.print(" : ");
-	Serial.println("ImpulseStop");
-	digitalWrite(OUTPUT1_PIN, LOW);
-	tImpulseOn.restartDelayed(WAIT_MS_MIN + random(WAIT_MS_MAX-WAIT_MS_MIN));
+	Serial.print("OFF ");
+	Serial.println(pin);
+	digitalWrite(pin, LOW);
 }
 
 void setup() 
 {
 	Serial.begin(115200);
+
+	Serial.print("Total time ~: ");
+	Serial.print((IMPULSE_MS_MAX - IMPULSE_MS_MIN + WAIT_MS_MAX - WAIT_MS_MIN) / 1000.0 * RETRIES / 60.0 / 60.0);
+	Serial.println(" hours");
+	
+
+
+
 	pinMode(OUTPUT1_PIN, OUTPUT);
+	pinMode(OUTPUT2_PIN, OUTPUT);
 	randomSeed(analogRead(0) + millis());
 }
 
