08fa9d8a6518fd294009683da22eafa6d31424ee
Dontsov Evgeny <don-and-home@mail.ru>
2020-06-18 17:48:25 +0300

    esp, attiny84 works

---
+++ ESP8266/src/voltage.h
@@ -0,0 +1,51 @@
+#include "setup.h"
+#include "Logging.h"
+#include "master_i2c.h"
+
+#ifdef WATERIUS_4C2W
+
+#define LOW_VOLTAGE 3300
+#define CRITICAL_VOLTAGE 3200
+
+uint32_t measure_voltage()
+{
+    uint32_t level = analogRead(A0);
+    LOG_INFO(FPSTR(S_ESP), F("A0 level: ") << level);
+    return 3.0 * level / 1023 * 10.0 / 3.0;   //100k + 30k resistors
+}   
+
+
+bool check_voltage(SlaveData &data, CalculatedData &cdata)
+{
+    uint32_t prev = data.voltage;
+    
+    data.voltage = measure_voltage();
+    uint32_t diff = abs(prev - data.voltage);
+    if (diff > cdata.voltage_diff) {
+        cdata.voltage_diff = diff;
+    }
+    
+    cdata.low_voltage = data.voltage < LOW_VOLTAGE;
+    return data.voltage < CRITICAL_VOLTAGE;
+}
+
+#else
+
+#define LOW_BATTERY_DIFF_MV 50  //надо еще учесть качество замеров (компаратора у attiny)
+#define ALERT_POWER_DIFF_MV 100
+
+bool check_voltage(SlaveData &data, CalculatedData &cdata)
+{   
+    uint32_t prev = data.voltage;
+	if (masterI2C.getSlaveData(data)) {
+        uint32_t diff = abs(prev - data.voltage);
+        if (diff > cdata.voltage_diff) {
+            cdata.voltage_diff = diff;
+        }
+        
+        cdata.low_voltage = cdata.voltage_diff >= LOW_BATTERY_DIFF_MV;
+        return cdata.voltage_diff < ALERT_POWER_DIFF_MV;
+	}
+    return true; //пропустим если ошибка i2c
+}
+#endif
