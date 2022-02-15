6c43ff7789c24e57e79a0a8f66e96b420044c367
Evgeny Dontsov <don-and-home@mail.ru>
2022-02-16 00:19:39 +0300

    Revert "Флаг отправки #190 Автоматическая подстройка периода отправки #209"

--- Attiny85/src/main.cpp
+++ Attiny85/src/main.cpp
@@ -232,11 +232,7 @@ void loop() {
 	} else {
 
 		LOG(F("wake up for transmitting"));
-		if (wdt_count < wakeup_period){
-			slaveI2C.begin(MANUAL_TRANSMIT_MODE);
-		}else{
-			slaveI2C.begin(TRANSMIT_MODE);
-		}
+		slaveI2C.begin(TRANSMIT_MODE);
 		wake_up_limit = WAIT_ESP_MSEC; //15 секунд при передаче данных
 	}
 
