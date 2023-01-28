c0100e3da9cb2bb4c7b6925efc358fe1c2b7666b
vzagorovskiy <vzagorovskiy@gmail.com>
2023-01-28 12:12:24 +0300

    При настроенной интеграции с HA данные будут отпралвться в один топик в формате JSON

--- ESP8266/src/sender_mqtt.h
+++ ESP8266/src/sender_mqtt.h
@@ -23,43 +23,10 @@
 #include "master_i2c.h"
 #include "Logging.h"
 #include "json.h"
-#include "ha_discovery.h"
+#include "ha/discovery.h"
+#include "ha/clean.h"
+#include "ha/helpers.h"
 #include "utils.h"
-/**
- * @brief Пубикует показания в один топик в формате json
- * 
- * @param mqtt_client клиент MQQT
- * @param topic имя топика
- * @param json_data данные в JSON
- */
-void publish_to_single_topic(PubSubClient &mqtt_client, String &topic,DynamicJsonDocument &json_data)
-{
-            LOG_INFO(F("MQTT: Publish data to single topic"));
-            String payload = "";
-            serializeJson(json_data, payload);
-            publish(mqtt_client, topic, payload);
-
-}
-/**
- * @brief публикация показаний в отдельные топики 
- * 
- * @param mqtt_client клиент MQQT
- * @param topic имя топика
- * @param json_data данные в JSON
- */
-void publish_to_multiple_topics(PubSubClient &mqtt_client, String &topic,DynamicJsonDocument &json_data)
-{
-            LOG_INFO(F("MQTT: Publish data to multiple topics"));
-            JsonObject root = json_data.as<JsonObject>();
-            for (JsonPair p : root)
-            {
-                LOG_INFO(F("Free memory: ") << ESP.getFreeHeap());
-                String sensor_topic = topic + "/" + p.key().c_str();
-                String sensor_value = p.value().as<String>();
-                publish(mqtt_client, sensor_topic, sensor_value);
-            }
-
-}
 
 
 /**
@@ -69,14 +36,13 @@ void publish_to_multiple_topics(PubSubClient &mqtt_client, String &topic,Dynamic
  * @param data показания
  * @param cdata расчитанные показатели
  * @param json_data json документ c показаниями
- * @param single_topic если true все данные будут отправляться одним запросом в один топик в формате json
  * @param auto_discovery если true то будет добавляться топик для автоконфигурации
  *
  * @returns true если успешно отправлены данные и false если не отправлено
  */
-bool send_mqtt(const Settings &sett, const SlaveData &data, const CalculatedData &cdata, DynamicJsonDocument &json_data, bool single_topic = true, bool auto_discovery = true)
+bool send_mqtt(const Settings &sett, const SlaveData &data, const CalculatedData &cdata, DynamicJsonDocument &json_data, bool auto_discovery = true)
 {
-    if (!sett.mqtt_host[0])
+    if (!is_mqtt(sett))
     {
         LOG_INFO(F("MQTT: SKIP"));
         return false;
@@ -95,22 +61,23 @@ bool send_mqtt(const Settings &sett, const SlaveData &data, const CalculatedData
     const char *login = sett.mqtt_login[0] ? sett.mqtt_login : nullptr;
     const char *pass = sett.mqtt_password[0] ? sett.mqtt_password : nullptr;
 
-    String topic = sett.mqtt_topic;
+    String mqtt_topic = sett.mqtt_topic;
 
     // Убираем слэш в конце
-    if (topic.endsWith("/"))
-        topic.remove(topic.length() - 1);
-
+    if (mqtt_topic.endsWith(F("/"))){
+        mqtt_topic.remove(mqtt_topic.length() - 1);
+    }
+        
     if (mqtt_client.connect(client_id.c_str(), login, pass))
     {
         unsigned long start = millis();
-        if (single_topic)
+        if (auto_discovery)
         {
-            publish_to_single_topic(mqtt_client, topic,json_data);
+            publish_data_to_single_topic(mqtt_client, mqtt_topic,json_data);
         }
         else
         {
-            publish_to_multiple_topics(mqtt_client, topic,json_data);
+            publish_data_to_multiple_topics(mqtt_client, mqtt_topic,json_data);
         }
         LOG_INFO(F("MQTT: Publish data finished. ") << millis() - start << F(" milliseconds elapsed"));
 
@@ -125,10 +92,10 @@ bool send_mqtt(const Settings &sett, const SlaveData &data, const CalculatedData
             if (sett.mode == SETUP_MODE) {
                 // Если находимся в режиме настройки 
                 //то предварительно очищаем все предыдущие автодискавери топики
-                clean_discovery(mqtt_client, topic);
+                clean_discovery(mqtt_client, mqtt_topic);
             }
             // Автоматическое добавления устройства в Home Assistant
-            publish_discovery(mqtt_client, topic, data, single_topic);
+            publish_discovery(mqtt_client, mqtt_topic, data);
         }
 
         mqtt_client.disconnect();
