7a0cc08a942f6760246dcda31ffdd9d37dffa34a
vzagorovskiy <vzagorovskiy@gmail.com>
2023-01-28 12:09:25 +0300

    Добавлены значения по умолчанию для инициализации

--- ESP8266/src/master_i2c.h
+++ ESP8266/src/master_i2c.h
@@ -20,7 +20,7 @@
 
 enum Status_t
 {
-    WATERIUS_NO_LINK = 0, //нет связи по i2c
+    WATERIUS_NO_LINK = 0, // нет связи по i2c
     WATERIUS_OK = 1,
     WATERIUS_BAD_CRC = 2
 };
@@ -31,28 +31,28 @@ enum Status_t
 struct SlaveData
 {
     // Header
-    uint8_t version;    //Версия ПО Attiny
-    uint8_t service;    //Причина загрузки Attiny
-    uint16_t reserved4; //Напряжение питания в мВ (после включения wi-fi под нагрузкой )
-    uint8_t reserved;
-    uint8_t setup_started_counter;
-    uint8_t resets;
-    uint8_t model;      // WATERIUS_CLASSIC или  WATERIUS_4C2W
-    uint8_t state0;     //Состояние, вход 0
-    uint8_t state1;     //           вход 1
-    uint32_t impulses0; //Импульсов, канал 0
-    uint32_t impulses1; //           канал 1
-    uint16_t adc0;      //Уровень,   канал 0
-    uint16_t adc1;      //           канал 1
+    uint8_t version = 0;    // Версия ПО Attiny
+    uint8_t service = 0;    // Причина загрузки Attiny
+    uint16_t reserved4 = 0; // Напряжение питания в мВ (после включения wi-fi под нагрузкой )
+    uint8_t reserved = 0;
+    uint8_t setup_started_counter = 0;
+    uint8_t resets = 0;
+    uint8_t model = WATERIUS_CLASSIC; // WATERIUS_CLASSIC или  WATERIUS_4C2W
+    uint8_t state0 = 0;               // Состояние, вход 0
+    uint8_t state1 = 0;               //           вход 1
+    uint32_t impulses0 = 0;           // Импульсов, канал 0
+    uint32_t impulses1 = 0;           //           канал 1
+    uint16_t adc0 = 0;                // Уровень,   канал 0
+    uint16_t adc1 = 0;                //           канал 1
 
     // HEADER_DATA_SIZE
 
-    uint8_t crc; //Всегда в конце структуры данных
-    uint8_t reserved2;
+    uint8_t crc = 0; // Всегда в конце структуры данных
+    uint8_t reserved2 = 0;
 
-    enum Status_t diagnostic;
-    uint8_t reserved3;
-    //Кратно 16bit https://github.com/esp8266/Arduino/issues/1825
+    enum Status_t diagnostic = WATERIUS_NO_LINK;
+    uint8_t reserved3 = 0;
+    // Кратно 16bit https://github.com/esp8266/Arduino/issues/1825
 };
 
 uint8_t crc_8(const unsigned char *input_str, size_t num_bytes, uint8_t crc = 0);
